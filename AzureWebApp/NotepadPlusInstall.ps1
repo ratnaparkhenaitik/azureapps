c2cbfd8495c1832296102126410803997ff4bb20
ratnaparkhenaitik <61557357+ratnaparkhenaitik@users.noreply.github.com>
2021-05-28 20:38:03 +0530

    Update NotepadPlusInstall.ps1

--- AzureWebApp/NotepadPlusInstall.ps1
+++ AzureWebApp/NotepadPlusInstall.ps1
@@ -1,3 +1,10 @@
 #InstallNotepadplusplus
+
+if(-not(Test-Path -Path C:\temp))
+{
+
+    New-Item -Path C:\temp -ItemType Directory
+}
 Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
 Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
+
